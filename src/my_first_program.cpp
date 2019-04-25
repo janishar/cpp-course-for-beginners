0af8b2bf32350bd378790d18ec803bf3007d95ef
Janishar Ali <janishar.ali@gmail.com>
2019-04-26 01:24:48 +0530

    fix comments

--- src/my_first_program.cpp
+++ src/my_first_program.cpp
@@ -1,11 +1,11 @@
 // my_first_program.cpp -- displays a message
 
-#include <iostream> 								// a PREPROCESSOR directive
+#include <iostream>                           // a PREPROCESSOR directive
 
-int main()											// function header
-{													// start of function body
-	using namespace std;							// make definitions visible
-	cout << "Hey! I am your first program";			// start a new line
-	cout << endl; 									// message
-	return 0;										// terminate main()
-} 													// end of function body
+int main()                                    // function header
+{                                             // start of function body
+	using namespace std;                      // make definitions visible
+	cout << "Hey! I am your first program";   // start a new line
+	cout << endl;                             // message
+	return 0;                                 // terminate main()
+}                                             // end of function body
